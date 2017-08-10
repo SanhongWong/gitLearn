43ee60252b866abbd73c1bf93da824921c51f334
SanhongWong <891910806@qq.com>
2017-08-10 15:23:34 +0800

    add one config

--- gitAlias.sh
+++ gitAlias.sh
@@ -4,3 +4,4 @@ git config --global alias.ci commit;
 git config --global alias.st status;
 git config --global alias.unstage 'reset HEAD --';
 git config --global alias.last 'log -1 HEAD';
+git config --global credential.helper cache
